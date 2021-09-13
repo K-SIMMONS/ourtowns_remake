1fe31a465201c18f87a60c22675c16d50207fb1a
Stephen Russell <srussell@alphaworks.tech>
2021-09-13 09:36:29 -0700

    feat(single-blog-page):title and category display correctly

--- single.php
+++ single.php
@@ -9,18 +9,25 @@
 
 get_header();
 ?>
- <div class = "container-fluid p-0">
-	<div class = "row">
-		<div class = "col-12 col-md-7 single-post-col-left">
-			
+
+<div class="container mt-5 mb-5">
+<?php 
+	$category = get_the_category();
+	echo '<a class="single-category-text" href="' . esc_url( get_category_link( $categories[0]->term_id) ) . '">'
+	 . $category[0]->name . '</a>'; ?>
+        <h1 class="single-page-title mt-4"><?php the_title();?></h1>
+        <hr class="title-hr">
+    </div>
+
+ <div class = "container">
+	 <div class="row justify-content-between">
+		<div class = "col-lg-8 col-md-7">
+			<?php the_content();?>
 		</div>
 	</div>
+</div>
 
-
-
-	</div>
-	<?php the_content();?>
 
 <?php
-get_sidebar();
+// get_sidebar();
 get_footer();
