5d6b0ac431c561e7e1f84ab83cf3ed0d16466852
Kurt Simmons <ksimmons@alphaworks.tech>
2021-09-22 17:02:21 -0700

    transafered latest to index.php and cleaned up code

--- index.php
+++ index.php
@@ -1,57 +1,43 @@
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package nokap_ourtown_remake
- */
+<?php get_header(); ?>
 
-get_header();
-?>
+<div class="container-fluid">
+       <div class="row">
+           <div class="col-3 text-center ms-3">
+               <h1 class="all_reports">All Reports</h1>
+            </div>
+        </div>
+    </div>
 
-	<main id="primary" class="site-main">
+<div class = "container mt-5">
+<div class = "row">
 
-		<?php
-		if ( have_posts() ) :
+<?php while(have_posts()) { 
+    
+    the_post();
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
+    $category = get_the_category(); ?>
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+    <div class = "col-sm-6 col-md-3">
+		<a class = "latest-block" href = "<?php the_permalink(); ?>">
+        <?php the_post_thumbnail('full' , array('class' => 'img-fluid'));?>
+        <p class = "latest-post-category"><?php echo $category[0]->name ?></p>
+        <h1 class = "latest-post-title"><?php the_title();?></h1>
+		</a>
+    </div>
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
 
-			endwhile;
 
-			the_posts_navigation();
 
-		else :
+<?php } ?> 
 
-			get_template_part( 'template-parts/content', 'none' );
 
-		endif;
-		?>
 
-	</main><!-- #main -->
 
-<?php
-get_sidebar();
-get_footer();
+</div>
+</div>
+
+
+
+
+
+<?php get_footer();?>
